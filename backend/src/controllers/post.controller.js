313a24b32bb873dedea540adce659d00d2d4be82
Fawaj Suraim <fawajsuraim@gmail.com>
2026-05-18 02:18:03 +0600

    feat: added CRUD APIs

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -67,8 +67,28 @@ const updatePost = async (req, res) => {
     }
 }
 
+const deletePost = async (req, res) => {
+    try {
+        const deleted = await Post.findByIdAndDelete(req.params.id);
+
+        if (!deleted) return res.status(404).json({
+            message: "Post not found!"
+        })
+
+        res.status(200).json({
+            message: "Post deleted successfully."
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error",
+            error
+        });
+    }
+}
+
 export {
     createPost,
     getPosts,
-    updatePost
+    updatePost,
+    deletePost
 };
